a494623f2c5ba018bb60a7415eb69ace85be4c1c
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2023-08-07 13:55:37 +0900

    feature(board.controller): add Board controller 1. GET, POST, PETCH, DELETE

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,12 +1,44 @@
-import { Controller, Get } from '@nestjs/common'
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Patch
+} from '@nestjs/common'
 import { BoardsService } from './boards.service'
+import { Board, BoardStatus } from './board.model'
+import { CreateBoardDto } from './dto/create-board.dto'
 
 @Controller('boards')
 export class BoardsController {
   constructor(private boardService: BoardsService) {}
 
   @Get()
-  getAllBoard() {
+  getAllBoard(): Board[] {
     return this.boardService.getAllBoards()
+  }
+
+  @Get('/:id')
+  getBoardById(@Param('id') id: string): Board {
+    return this.boardService.getBoardById(id)
+  }
+
+  @Post()
+  createBoard(@Body() createBoardDto: CreateBoardDto): Board {
+    return this.boardService.createBoard(createBoardDto)
+  }
+
+  @Patch('/:id/status')
+  updateBoardStatus(
+    @Param('id') id: string,
+    @Body('status') status: BoardStatus
+  ): Board {
+    return this.boardService.updateBoardStatus(id, status)
+  }
+  @Delete('/:id')
+  deleteBoardById(@Param('id') id: string): void {
+    this.boardService.deleteBoardById(id)
   }
 }
