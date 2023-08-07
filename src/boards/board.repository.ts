8c8af80f56dbcd96ee5ceab98adac87ae2fb0e92
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2023-08-07 16:57:31 +0900

    refactor(board.repository): replace dataSource

--- src/boards/board.repository.ts
+++ src/boards/board.repository.ts
@@ -3,14 +3,15 @@ import { Board } from './board.entity'
 import { InjectRepository } from '@nestjs/typeorm'
 import { CreateBoardDto } from './dto/create-board.dto'
 import { BoardStatus } from './board-status.enum'
+import { Injectable } from '@nestjs/common'
 
+@Injectable()
 export class BoardRepository extends Repository<Board> {
-  constructor(@InjectRepository(Board) private dataSource: DataSource) {
-    super(Board, dataSource.manager)
+  constructor(private dataSource: DataSource) {
+    super(Board, dataSource.createEntityManager())
   }
   async createBoard(createBoard: CreateBoardDto): Promise<Board> {
     const { title, description } = createBoard
-
     const board = this.create({
       title,
       description,
