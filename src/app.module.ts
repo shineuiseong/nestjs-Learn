b468f5a3513e044ef5a82bc57a9bfe50e103f310
shineuiseong <30334829+shineuiseong@users.noreply.github.com>
2023-08-08 15:43:41 +0900

    feature(app.module): add env ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
+import appConfig from './configs/app.config'
 import { BoardsModule } from './boards/boards.module'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { typeORMConfig } from './configs/typeorm.config'
-
+import process from 'process'
+import path from 'path'
 @Module({
-  imports: [TypeOrmModule.forRoot(typeORMConfig), BoardsModule]
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig],
+      envFilePath: path.resolve(
+        __dirname,
+        '..',
+        'src',
+        'env',
+        `${process.env.NODE_ENV}` === 'production'
+          ? '.env.production'
+          : '.env.development'
+      )
+    }),
+    TypeOrmModule.forRoot(typeORMConfig),
+    BoardsModule
+  ]
 })
 export class AppModule {}
